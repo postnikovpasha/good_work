905c81ee8a1572c92da85ce10802b59f5f091884
Andrey <morozoffandrey-m@yandex.u>
2019-01-29 11:46:24 +0700

    Сделал дизайн кнопки добавления и икноку

--- app/src/main/java/com/example/autapplication/SetActivity.kt
+++ app/src/main/java/com/example/autapplication/SetActivity.kt
@@ -4,10 +4,11 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_set.*
+import android.support.design.widget.BottomNavigationView
+
+
 
 class SetActivity : AppCompatActivity() {
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +22,7 @@ class SetActivity : AppCompatActivity() {
                     true
                 }
                 R.id.action_settings -> {
+
                     val intent = Intent(this, SetActivity::class.java)
                     startActivity(intent)
                     true
