7ccddd7d859a910011e5e000a6e32bccb325f2cc
Pavel <postnikovpasha@mail.ru>
2019-01-30 21:31:37 +0700

    Auth активити, подчеркивания

--- app/src/main/java/com/example/autapplication/AuthActivity.kt
+++ app/src/main/java/com/example/autapplication/AuthActivity.kt
@@ -15,6 +15,7 @@ class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
+        getSupportActionBar()!!.hide();
 
         btnAuth.setOnClickListener {
             val login = loginEditText.text.toString()
