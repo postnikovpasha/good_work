756bf6708154df20336092d3e175028b0bbd50bc
Pavel <postnikovpasha@mail.ru>
2019-01-31 15:15:44 +0700

    Скрытие fab при скроле

--- app/src/main/java/com/example/autapplication/HomeFragment.kt
+++ app/src/main/java/com/example/autapplication/HomeFragment.kt
@@ -50,6 +50,22 @@ class HomeFragment : Fragment(), WorksView {
 
         val recyclerView = worksRecyclerView
 
+        recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                if (dy > 0 || dy < 0 && fab.isShown) {
+                    fab.hide()
+                }
+            }
+
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                if (newState == RecyclerView.SCROLL_STATE_IDLE) {
+                    fab.show()
+                }
+
+                super.onScrollStateChanged(recyclerView, newState)
+            }
+        })
+
     }
 
     companion object {
